84bdb3b8133cb5f073f7edebf9dc60ba93d95b27
Christopher LaChance <chris@cart.com>
2022-09-12 16:49:16 -0500

    Super basic checkov test.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# checkov-policies
+# Checkov Policies
 A repository to house custom Checkov policies for our Terraform code.
+
+## Installation
+
+Developing custom policy checks doesn't necessarily require you to install anything, as they are run entirely in our GitHub Action pipelines via the official Checkov action (https://github.com/bridgecrewio/checkov-action). However,if you want proper intellisense or code completion to operate via your IDE, you can install the `checkov` module via PIP.
+
+```
+#MacOS via Homebrew
+brew install python3
+
+#Ubuntu/Debian
+sudo apt-get install python3-pip 
+
+pip3 install checkov
+```
---
+++ l2/NamingSchema.py
@@ -0,0 +1,24 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class NamingSchema(BaseResourceCheck):
+    def __init__(self):
+        name = "Validate all Google resource names according to schema."
+        id = "CKV_GCP_1000"
+        supported_resources = ["google_*"]
+        categories = [CheckCategories.CONVENTION]
+        super().__init__(name=name, id=id, categories=categories,
+                         supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if 'labels' in conf.keys():
+            return CheckResult.PASSED
+
+        self.details.append(
+            """ No 'labels' block was defined for the Google resource!
+                Labels are utilized for dynamic tagging and are required
+                for auditing purposes. The schema for resource labels can be
+                found in the organization README file.
+            """)
+        return CheckResult.FAILED

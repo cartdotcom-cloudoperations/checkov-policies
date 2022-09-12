ad2d7e629c20961d155f10b48de3ec7f652ada3f
Christopher LaChance <chris@cart.com>
2022-09-12 18:45:34 -0500

    Testing updates to LabelsSchema.

--- l2/GCPLabelsSchema.py
+++ l2/GCPLabelsSchema.py
@@ -1,10 +1,9 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
-
-class NamingSchema(BaseResourceCheck):
+class GCPLabelsSchema(BaseResourceCheck):
     def __init__(self):
-        name = "Validate all Google resource names according to schema."
+        name = "Validate all Google resources have labels according to schema."
         id = "CKV_GCP_1000"
         supported_resources = ["google_*"]
         categories = [CheckCategories.CONVENTION]
@@ -14,14 +13,14 @@ class NamingSchema(BaseResourceCheck):
     def scan_resource_conf(self, conf):
         if 'labels' in conf.keys():
             return CheckResult.PASSED
+        else:
+            self.details.append(
+                """No 'labels' block was defined for the Google resource!
+              Labels are utilized for dynamic tagging and are required
+              for auditing purposes. The schema for resource labels can be
+              found in the organization README file.
+              """)
+            return CheckResult.FAILED
 
-        self.details.append(
-            """ No 'labels' block was defined for the Google resource!
-                Labels are utilized for dynamic tagging and are required
-                for auditing purposes. The schema for resource labels can be
-                found in the organization README file.
-            """)
-        return CheckResult.FAILED
 
-
-scanner = NamingSchema()
+scanner = GCPLabelsSchema()

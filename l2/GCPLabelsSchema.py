a50639ce7b86e40103f9e783b3ec6f6e83eb2378
Christopher LaChance <chris@cart.com>
2022-09-12 19:06:27 -0500

    Tweaked string literal to set.

--- l2/GCPLabelsSchema.py
+++ l2/GCPLabelsSchema.py
@@ -15,11 +15,11 @@ class GCPLabelsSchema(BaseResourceCheck):
         if 'labels' in conf.keys():
             return CheckResult.PASSED
         else:
-            self.details = """No 'labels' block was defined for the Google resource!
+            self.details = set("""No 'labels' block was defined for the Google resource!
             Labels are utilized for dynamic tagging and are required
             for auditing purposes. The schema for resource labels can be
             found in the organization README file.
-            """
+            """)
             return CheckResult.FAILED
 
 
